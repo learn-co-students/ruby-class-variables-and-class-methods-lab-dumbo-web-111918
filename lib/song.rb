9b925cf6a9988ca48f56442da337d9ff948ec783
mwestervelt <elle.westervelt@gmail.com>
2018-11-23 13:47:41 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+
+  attr_accessor :name, :artist, :genre, :count, :artists, :genres, :genre_count, :artist_count
+
+####  class variables  ####
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@genre_count = {}
+    @@artist_count = {}
+
+####  initialize  ####
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+
+    @@genres = @@genres
+      if @@genre_count.keys.include?(@genre)
+        @@genre_count[@genre] += 1
+      else @@genre_count[@genre] = 1
+    end
+
+    @@artists = @@artists
+      if @@artist_count.keys.include?(@artist)
+        @@artist_count[@artist] += 1
+      else @@artist_count[@artist] = 1
+    end
+  end
+
+####  class methods  ####
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artist_count
+  end
+
+end
