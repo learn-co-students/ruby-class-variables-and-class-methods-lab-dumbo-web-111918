2834f00baa4fe9073c1cf36cbf523be52f832cf8
github username <github email address>
2018-11-27 16:24:49 -0500

    Done.

---
+++ run.rb
@@ -0,0 +1,12 @@
+require_relative "lib/song.rb"
+require 'pry'
+
+
+chanel = Song.new("chanel", "frank ocean", "hip hop")
+snow = Song.new("snow", "rhcp", "funk")
+walk = Song.new("walk", "foo fighters", "rock")
+everlong = Song.new("everlong", "foo fighters", "rock")
+
+
+
+binding.pry
